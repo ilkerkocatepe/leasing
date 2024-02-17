1d7f9c352db0feb013299f82273280dd78b374bc
ilkerkocatepe kocatepeilker@gmail.com <kocatepeilker@gmail.com>
2024-02-17 14:04:45 +0300

    update CORS config

--- src/main/java/dev/ilkerk/leasing/infrastracture/security/CorsConfig.java
+++ src/main/java/dev/ilkerk/leasing/infrastracture/security/CorsConfig.java
@@ -13,7 +13,7 @@ public class CorsConfig {
     @Bean
     CorsWebFilter corsWebFilter() {
         var corsConfiguration = new CorsConfiguration();
-        corsConfiguration.setAllowedOrigins(List.of("http://localhost:4200"));
+        corsConfiguration.setAllowedOrigins(List.of("http://localhost:4200", "http://dev.ilkerkocatepe.com.tr"));
         corsConfiguration.setAllowedMethods(List.of("*"));
         corsConfiguration.setAllowedHeaders(List.of("content-type", "Access-Control-Request-Method", "Access-Control-Request-Headers", "Access-Control-Allow-Origin"));
         corsConfiguration.setAllowCredentials(true);
