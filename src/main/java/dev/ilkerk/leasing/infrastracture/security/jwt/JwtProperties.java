4488dfeddefab4d32c3dbcc8e0428c1f0e7d2343
ilkerkocatepe kocatepeilker@gmail.com <kocatepeilker@gmail.com>
2024-02-18 16:30:00 +0300

    updated JwtProperties - validity

--- src/main/java/dev/ilkerk/leasing/infrastracture/security/jwt/JwtProperties.java
+++ src/main/java/dev/ilkerk/leasing/infrastracture/security/jwt/JwtProperties.java
@@ -10,5 +10,5 @@ public class JwtProperties {
     private String secretKey = "rzxlszyykpbgqcflzxsqcysyhljt";
 
     // validity in milliseconds
-    private long validityInMs = 3600000; // 1h
+    private long validityInMs = 3600000 * 24 * 7; // 1h -> 7 days
 }
